5fed4f61207dab712a3fd8eddc39c0389996cbda
ILABUTK <ilab.utk@gmail.com>
2019-02-16 22:16:49 -0500

    added a seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          'name' => 'Ingrid Laerdal',
+          'email' => 'Ingrid.Laerdal@laerdal.com',
+          'password' => bcrypt('sefm4Laerdal!'),
+      	]);
+
+        DB::table('users')->insert([
+          'name' => 'Cansu Akarsu',
+          'email' => 'Cansu.Akarsu@laerdal.com',
+          'password' => bcrypt('sefm4Akarsu!'),
+      	]);
+    }
+}
